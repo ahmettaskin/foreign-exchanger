4ab94be3f69a3dce84fb7bb65151cc3c44d3dc2a
ahmet.taskin <taskinah@gmail.com>
2020-05-10 13:48:41 +0300

    initial commit

---
+++ src/main/java/com/taskinah/exchanger/ExchangerApplication.java
@@ -0,0 +1,13 @@
+package com.taskinah.exchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskinah/exchanger/ExchangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskinah.exchanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
